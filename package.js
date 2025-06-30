9831bf6a6001fc165fd973efe46693c81d731522
talktoudom <udomowodiong@gmail.com>
2025-06-30 12:38:13 +0100

    Add files via upload

---
+++ package.js
@@ -0,0 +1,13 @@
+{
+  "name": "whatsapp-quizbot",
+  "version": "1.0.0",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "body-parser": "^1.20.2",
+    "dotenv": "^16.0.3",
+    "express": "^4.18.2"
+  }
+}
